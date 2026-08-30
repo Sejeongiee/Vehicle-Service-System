4ec9ed36bbbb7e98b59b81af9bf5c1304e778216
Sejeongiee <kindredjebgold@gmail.com>
2026-08-30 22:21:00 +0800

    update index.php in admin

--- vehicle-service-management-admin/index.php
+++ vehicle-service-management-admin/index.php
@@ -1,7 +1,33 @@
 <?php
 
-header("Location: login.php");
+include "includes/config.php";
 
-exit();
 
-?>
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+if (
+    isset($_SESSION['admin_id']) &&
+    ($_SESSION['admin_role'] ?? '') === 'staff'
+) {
+
+    header(
+        "Location: "
+        . ADMIN_BASE_URL
+        . "/dashboard.php"
+    );
+
+} else {
+
+    header(
+        "Location: "
+        . ADMIN_BASE_URL
+        . "/login.php"
+    );
+
+}
+
+
+exit;
