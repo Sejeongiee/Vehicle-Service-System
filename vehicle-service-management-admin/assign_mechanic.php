5bd2101dd600c7a5fd80c80ce3c05caa9c3a0c08
Sejeongiee <kindredjebgold@gmail.com>
2026-09-03 10:58:37 +0800

    fix includes order in reservations and assign mechanic

--- vehicle-service-management-admin/assign_mechanic.php
+++ vehicle-service-management-admin/assign_mechanic.php
@@ -1,7 +1,7 @@
 <?php
 
-include "includes/admin_auth.php";
-include "includes/config.php";
+include_once __DIR__ . "/includes/config.php";
+include_once __DIR__ . "/includes/admin_auth.php";
 
 
 /*
--- vehicle-service-management-admin/reservation_view.php
+++ vehicle-service-management-admin/reservation_view.php
@@ -1,7 +1,7 @@
 <?php
 
-include "includes/admin_auth.php";
-include "includes/config.php";
+include_once __DIR__ . "/includes/config.php";
+include_once __DIR__ . "/includes/admin_auth.php";
 
 $reservation_id = intval($_GET['id'] ?? 0);
 
