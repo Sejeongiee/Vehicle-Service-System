547c1d4332e83a92dbcf432b2020bbdd41fddb3d
teddygillico <teddygil.lico@neu.edu.ph>
2026-09-06 17:09:01 +0800

    TG update admin header

--- vehicle-service-management-admin/includes/admin_header.php
+++ vehicle-service-management-admin/includes/admin_header.php
@@ -15,7 +15,7 @@ include_once __DIR__ . "/admin_auth.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>
-        Vehicle Service Management Admin
+        LUCKY YUNA CAR CARE CENTER - Admin
     </title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -35,7 +35,7 @@ include_once __DIR__ . "/admin_auth.php";
 
             <div class="sidebar-logo">
 
-                🚗 VSMS Admin
+                🚗 LYC Admin
 
             </div>
 
@@ -84,9 +84,10 @@ include_once __DIR__ . "/admin_auth.php";
                 </li>
 
                 <li>
-                    <a href="<?= ADMIN_BASE_URL ?>/logout.php">
-                        🚪 Logout
-                    </a>
+                    <a href="<?= ADMIN_BASE_URL ?>/logout.php"
+   style="background-color: #dc2626; color: white; font-weight: bold; padding: 10px 18px; text-decoration: none; border-radius: 6px; display: inline-block;">
+    🚪 Logout
+</a>
                 </li>
 
             </ul>
